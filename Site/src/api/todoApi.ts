dbf56ebe04fc48ad6d789ae6137aaf6ef746488e
Trung Nguyen <trung.nguyen@groovetechnology.com>
2026-02-13 17:57:25 +0700

    Remote base Api Url.

--- Site/src/api/todoApi.ts
+++ Site/src/api/todoApi.ts
@@ -4,15 +4,13 @@ export interface Todo {
   isCompleted: boolean;
 }
 
-const API = import.meta.env.VITE_API_URL;
-
 export async function getTodos(): Promise<Todo[]> {
-  const r = await fetch(`${API}/api/todos`);
+  const r = await fetch(`/api/todos`);
   return r.json();
 }
 
 export async function createTodo(title: string) {
-  const r = await fetch(`${API}/api/todos`, {
+  const r = await fetch(`/api/todos`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(title),
@@ -21,9 +19,9 @@ export async function createTodo(title: string) {
 }
 
 export async function toggleTodo(id: string) {
-  await fetch(`${API}/api/todos/${id}/toggle`, { method: "PUT" });
+  await fetch(`/api/todos/${id}/toggle`, { method: "PUT" });
 }
 
 export async function deleteTodo(id: string) {
-  await fetch(`${API}/api/todos/${id}`, { method: "DELETE" });
+  await fetch(`/api/todos/${id}`, { method: "DELETE" });
 }
